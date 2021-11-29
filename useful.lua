c2597258f0a38a069737414d67493fbfa707b976
sw1tch <87098584+sw1tchh@users.noreply.github.com>
2021-11-29 18:53:29 +0300

    Update useful.lua
    update;
    fonksiyonlar değiştirildi, daha optimize ve basit hale getirildi.

--- useful.lua
+++ useful.lua
@@ -2,28 +2,73 @@
 	author: @sw1tch
 ]]--
 
-function checkPlayerAdmin(oyuncu)
-    local adminlevel = getElementData(oyuncu,"admin_level")
-    if adminlevel > 0 then return true end 
-    if adminlevel < 0 then return false end 
+function checkPlayerAdminLevel(player,level)
+	local level = tonumber(level)
+	local a_level = getElementData(player,"admin_level")
+	
+	if not level then 
+		if a_level > 0 then 
+			return true 
+		else 
+			return false 
+		end 
+	return end 
+	
+	if a_level == level then 
+		return true 
+	else 
+		return false  
+	end 
+	
 end 
 
-function checkPlayerSupporter(oyuncu)
-    local supportlevel = getElementData(oyuncu,"supporter_level")
-    if supportlevel > 0 then return true end 
-    if supportlevel < 0 then return false end 
+function checkPlayerSupporterLevel(player,level)
+	local level = tonumber(level)
+	local s_level = getElementData(player,"supporter_level")
+	
+	if not level then 
+		if s_level > 0 then 
+			return true 
+		else 
+			return false 
+		end 
+	return end 
+	
+	if s_level == level then 
+		return true
+	else 
+		return false 
+	end 
+	
 end 
-
-function checkPlayerStaff(oyuncu)
-    local adminlevel = getElementData(oyuncu,"admin_level")
-    local supportlevel = getElementData(oyuncu,"supporter_level")
-    if adminlevel > 0 or supportlevel > 0 then return true end 
-    if adminlevel < 0 or supportlevel < 0 then return false end 
-end 
-
 
 -- Kullanım Örneği
 
-function use(player)
-    if checkPlayerStaff(player) == true then return print("yetkilisin") end
+function use(oyuncu,cmd)
+
+	if checkPlayerAdminLevel(oyuncu,2) == true then
+		outputChatBox("Admin Level'in 2'ye eşit.", oyuncu)
+	else 
+		outputChatBox("Admin Level'in 2'ye eşit değil", oyuncu)
+	end 
+	
+	if checkPlayerSupporterLevel(oyuncu,1) == true then 
+		outputChatBox("Supporter levelin 1'e eşit.", oyuncu)
+	else 
+		outputChatBox("Supporter levelin 1'e eşit değil", oyuncu)
+	end 
+	
+	if checkPlayerAdminLevel(oyuncu) == true then 
+		outputChatBox("Adminsin.", oyuncu)
+	else 
+		outputChatBox("Admin değilsin.",oyuncu)
+	end 
+	
+	if checkPlayerSupporterLevel(oyuncu) == true then 
+		outputChatBox("Supportersın", oyuncu)
+	else 
+		outputChatBox("Supporter değilsin.",oyuncu)
+	end 
+	
 end 
+addCommandHandler("test", use)
